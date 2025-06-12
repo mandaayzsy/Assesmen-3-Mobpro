51f11209f14a0c3508143c8a7b1b2692ad066e6c
Manda Aysy Erina Bhakti <mandaayzsy@gmail.com>
2025-06-13 03:02:14 +0700

    Membuat model dan data store user serta model jam

---
+++ app/src/main/java/com/manda0101/assesmen3/model/Jam.kt
@@ -0,0 +1,8 @@
+package com.manda0101.assesmen3.model
+
+data class Jam(
+    val id_jam: Long = 0L,
+    val name: String,
+    val serialNumber: String,
+    val mine: String
+)
---
+++ app/src/main/java/com/manda0101/assesmen3/model/User.kt
@@ -0,0 +1,8 @@
+package com.manda0101.assesmen3.model
+
+data class User(
+    val token: String = "",
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+)
